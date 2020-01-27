fef62060ef390d559c07765b68b3c0d633b91ecb
Igor Balanici <igor.balanici@gmail.com>
2020-01-28 01:37:56 +0200

    Strategy pattern

---
+++ 10-strategy/src/StrategyDemo.java
@@ -0,0 +1,15 @@
+public class StrategyDemo {
+    public static void main(String[] args) {
+        CreditCard amexCard = new CreditCard(new AmexStrategy());
+        amexCard.setNumber("379185883464283");
+        amexCard.setDate("04/2020");
+        amexCard.setCvv("123");
+        System.out.println("is Amex valid: " + amexCard.isValid());
+
+        CreditCard visaCard = new CreditCard(new VisaStrategy());
+        visaCard.setNumber("4539589763663402");
+        visaCard.setDate("05/2018");
+        visaCard.setCvv("123");
+        System.out.println("is Visa valid: " + visaCard.isValid());
+    }
+}
