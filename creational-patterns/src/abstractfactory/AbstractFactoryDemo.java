baa9f9eafab8ce78ef0a14975696744ce14fa3ff
Igor Balanici <igor.balanici@gmail.com>
2020-02-20 00:54:28 +0200

    Abstract factory pattern

---
+++ creational-patterns/src/abstractfactory/AbstractFactoryDemo.java
@@ -0,0 +1,16 @@
+package abstractfactory;
+
+public class AbstractFactoryDemo {
+
+    public static void main(String[] args) {
+
+        CreditCardFactory abstractFactory = CreditCardFactory.getCreditCardFactory(775);
+        CreditCard creditCard = abstractFactory.getCreditCard(CardType.PLATINUM);
+        System.out.println(creditCard.getClass());
+
+        abstractFactory = CreditCardFactory.getCreditCardFactory(600);
+        CreditCard creditCard1 = abstractFactory.getCreditCard(CardType.GOLD);
+        System.out.println(creditCard1.getClass());
+
+    }
+}
---
+++ creational-patterns/src/abstractfactory/AmexGoldValidator.java
@@ -0,0 +1,11 @@
+package abstractfactory;
+
+public class AmexGoldValidator implements Validator {
+
+    @Override
+    public boolean isValid(CreditCard creditCard) {
+
+
+        return false;
+    }
+}
